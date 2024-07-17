48eb465973ebde0761e89509b4e4ec2ce71936f7
bananchik033 <d.belozerov06@mail.ru>
2024-07-17 20:23:13 +0200

    z3

--- z3/Program.cs
+++ z3/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int v = int.Parse(Console.ReadLine());
+Console.WriteLine($"{n}+{v}={n+v}");
