42998091f240e031d45ebf84211d03f38d7d5eea
mikhaa222555 <smplenavi23@gmail.com>
2024-07-17 21:07:26 +0200

    z4

---
+++ z4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Напишите число, что бы проверить является ли четное или не четное оно:");
+int h = int.Parse(Console.ReadLine()!);
+if(h % 2 == 0)
+{
+    Console.Write("Четное");
+}
+else 
+{
+    Console.Write("Не четное");
+}
