5a9fc571b2610593d91a0c45fc770a52122f0208
bananchik033 <d.belozerov06@mail.ru>
2024-07-17 21:01:36 +0200

    z10

--- z10/Program.cs
+++ z10/Program.cs
@@ -2,4 +2,6 @@
 int n = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int v = int.Parse(Console.ReadLine());
-Console.WriteLine($"{n}+{v}={n + v}");
+Console.Write("Введите третье число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine((n+v+b)/3);
