e709f642cf972b797fe00054725e3e1846887a56
bananchik033 <d.belozerov06@mail.ru>
2024-07-17 20:57:58 +0200

    z8

--- z8/Program.cs
+++ z8/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
+string stroka = Console.ReadLine();
+int xz = stroka.Length;
+Console.WriteLine("Колличество символов в строке: " + xz);
