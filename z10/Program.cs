e3faa1eb11f5694332791b693056246cf3d733a4
bananchik033 <d.belozerov06@mail.ru>
2024-07-17 21:00:08 +0200

    z10

---
+++ z10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int v = int.Parse(Console.ReadLine());
+Console.WriteLine($"{n}+{v}={n + v}");
