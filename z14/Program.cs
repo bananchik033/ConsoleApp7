3334fed0899b54f69c7ee1e746e1e3c0ff98f357
bananchik033 <d.belozerov06@mail.ru>
2024-07-17 21:21:02 +0200

    z14

---
+++ z14/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine());
+int n = 0;
+for (int i = 1; i<b; i++)
+{
+    if (b%i==0)
+    {
+        n += i;
+    }   
+}
+if (b != n) { Console.WriteLine("число несовершенное"); }
+else Console.WriteLine("Число совершенное");
+
